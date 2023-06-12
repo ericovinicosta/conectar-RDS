353a909e53497ab845da0d0997e4fce37646437a
suporteerico <suporte@ericocosta.com.br>
2023-06-12 01:31:49 -0300

    inserindo e listando

---
+++ src/br/com/ericocosta/conectards_aws/ConectaRDS_AWS.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.ericocosta.conectards_aws;
+
+import br.com.ericocosta.conectards_aws.JanelaApp;
+
+
+
+/**
+ *
+ * @author erico
+ */
+public class ConectaRDS_AWS  {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JanelaApp app = new JanelaApp("Conectar RDS");
+        app.setVisible(true);
+        
+    }
+    
+}
